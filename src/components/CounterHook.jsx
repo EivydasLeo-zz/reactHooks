f10b70d13a88f3d5d7570c58089efd7779c9f006
Your Name <you@example.com>
2021-08-24 14:00:31 +0300

    class comp side by side with hook component

--- src/components/CounterHook.jsx
+++ src/components/CounterHook.jsx
@@ -4,12 +4,17 @@ function CounterHook() {
   const arr = useState(0);
   console.log(arr);
   const counter = arr[0];
+  const setCounter = arr[1];
+
+  const onCounter = () => {
+    setCounter(counter + 1);
+  };
 
   return (
     <div>
       <h2>CounterHook</h2>
       <p>{counter}</p>
-      <button>Increase</button>
+      <button onClick={onCounter}>Increase</button>
     </div>
   );
 }
