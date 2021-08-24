449001cd2e40232e5f314397c10d5002c14f233f
Your Name <you@example.com>
2021-08-24 14:09:17 +0300

    destructuring useState

--- src/components/CounterHook.jsx
+++ src/components/CounterHook.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 
 function CounterHook() {
-  const arr = useState(0);
-  console.log(arr);
-  const counter = arr[0];
-  const setCounter = arr[1];
+  const [counter, setCounter] = useState(0);
+  // console.log(arr);
+  // const counter = arr[0];
+  // const setCounter = arr[1];
 
-  const onCounter = () => {
-    setCounter(counter + 1);
+  const obj = {
+    name: 'bob',
+    age: 55,
   };
+  const { name, age } = obj;
+  console.log(name, age);
+
+  const arr1 = [100, 'yes', true];
+  const [nr, answer, monday] = arr1;
+  console.log(nr, answer, monday);
 
   return (
     <div>
       <h2>CounterHook</h2>
       <p>{counter}</p>
-      <button onClick={onCounter}>Increase</button>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
     </div>
   );
 }
